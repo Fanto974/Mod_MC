b6b1df3d78689870fdbfd2cd6170fb13bc9d2d9e
mathis <mathis.buclon@efrei.net>
2025-04-01 18:58:22 +0200

    maj de l'arborescence

--- src/main/java/net/fanto974/firstmod/Config.java
+++ src/main/java/net/fanto974/firstmod/Config.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package net.fanto974.firstmod;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
--- src/main/java/net/fanto974/firstmod/ExampleMod.java
+++ src/main/java/net/fanto974/firstmod/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package net.fanto974.firstmod;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
